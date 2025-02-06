d71ba765e1fd31ef0ee4af6d65fdeceacf4f7e71
arunyeldi <arunyeldi@gmail.com>
2025-02-06 12:16:32 +0530

    files added

---
+++ day13/customdecorators.py
@@ -0,0 +1,17 @@
+import time
+
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        time.sleep(1)
+        print("Before the function call")
+        result = func(*args, **kwargs)
+        print("After the function call")
+        time.sleep(1)
+        return result
+    return wrapper
+
+@my_decorator
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Arun")
